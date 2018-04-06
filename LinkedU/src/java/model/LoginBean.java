80c941faf5d848d75ebccdb5e1bb399031c80976
jdfost1 <38138363+jdfost1@users.noreply.github.com>
2018-04-06 11:41:15 -0500

    Add files via upload

---
+++ LinkedU/src/java/model/LoginBean.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author it353s833
+ */
+@ManagedBean
+@RequestScoped
+public class LoginBean {
+    private String username;
+    private String password;
+    
+     /**
+     * Creates a new instance of LoginBean
+     */
+    public LoginBean() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+
+   
+    
+}
